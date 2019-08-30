db8103b48f5f7c7d9a339642f00e6e88f07752bd
Anton Lukin <mininzidane@gmail.com>
2019-08-31 03:33:11 +1000

    add the only host for admin module

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -2,7 +2,17 @@
 
 namespace app\modules\admin;
 
+use yii\web\HttpException;
+
 class Module extends \yii\base\Module
 {
     public $layout = '@app/modules/admin/views/layouts/default';
+
+    public function init()
+    {
+        if (\Yii::$app->request->hostName !== 'a.wowtoknow.com') {
+            throw new HttpException(401, 'Get the fuck out here');
+        }
+        parent::init();
+    }
 }
