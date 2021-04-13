d9afe50ad36f12827bf2629b47dce9e1c93b6a5f
Anton Lukin <mininzidane@gmail.com>
2021-04-13 11:41:30 +1000

    add try catch to translate command

--- commands/TranslateContentController.php
+++ commands/TranslateContentController.php
@@ -59,36 +59,42 @@ class TranslateContentController extends Controller
                     continue;
                 }
 
-                $contentSplitted = TextSplitter::chunkBySize($page->content, $this->translation->maxTextLength());
-                $translatedTexts = $this->translation->translate($contentSplitted, Creator::CONTENT_SOURCE_LANGUAGE, $page->site->target_language);
-                unset($contentSplitted);
-                if ($translatedTexts !== null) {
-                    $page->content = implode(' ', $translatedTexts);
-                } else {
-                    $this->writeLog("Could not translate content for page #{$page->id}");
+                try {
+                    $contentSplitted = TextSplitter::chunkBySize($page->content, $this->translation->maxTextLength());
+                    $translatedTexts = $this->translation->translate($contentSplitted, Creator::CONTENT_SOURCE_LANGUAGE, $page->site->target_language);
+                    unset($contentSplitted);
+                    if ($translatedTexts !== null) {
+                        $page->content = implode(' ', $translatedTexts);
+                    } else {
+                        $this->writeLog("Could not translate content for page #{$page->id}");
+                        continue;
+                    }
+
+                    $translation = $this->translation->translate([$page->title], Creator::CONTENT_SOURCE_LANGUAGE, $page->site->target_language);
+                    if ($translation !== null) {
+                        $page->title = $translation[0];
+                    } else {
+                        $this->writeLog("Could not translate title for page #{$page->id}");
+                    }
+                    $translation = $this->translation->translate([$page->keywords], Creator::CONTENT_SOURCE_LANGUAGE, $page->site->target_language);
+                    if ($translation !== null) {
+                        $page->keywords = $translation[0];
+                    } else {
+                        $this->writeLog("Could not translate keywords for page #{$page->id}");
+                    }
+                    $translation = $this->translation->translate([$page->description], Creator::CONTENT_SOURCE_LANGUAGE, $page->site->target_language);
+                    if ($translation !== null) {
+                        $page->description = $translation[0];
+                    } else {
+                        $this->writeLog("Could not translate description for page #{$page->id}");
+                    }
+
+                    $page->save();
+
+                } catch (\Throwable $e) {
+                    $this->writeLog("Error: {$e->getMessage()}");
                     continue;
                 }
-
-                $translation = $this->translation->translate([$page->title], Creator::CONTENT_SOURCE_LANGUAGE, $page->site->target_language);
-                if ($translation !== null) {
-                    $page->title = $translation[0];
-                } else {
-                    $this->writeLog("Could not translate title for page #{$page->id}");
-                }
-                $translation = $this->translation->translate([$page->keywords], Creator::CONTENT_SOURCE_LANGUAGE, $page->site->target_language);
-                if ($translation !== null) {
-                    $page->keywords = $translation[0];
-                } else {
-                    $this->writeLog("Could not translate keywords for page #{$page->id}");
-                }
-                $translation = $this->translation->translate([$page->description], Creator::CONTENT_SOURCE_LANGUAGE, $page->site->target_language);
-                if ($translation !== null) {
-                    $page->description = $translation[0];
-                } else {
-                    $this->writeLog("Could not translate description for page #{$page->id}");
-                }
-
-                $page->save();
             }
             $i++;
 
